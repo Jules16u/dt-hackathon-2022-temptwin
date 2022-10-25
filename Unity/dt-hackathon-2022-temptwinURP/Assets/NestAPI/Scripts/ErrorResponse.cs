db6299d891a509ac48c2bb1296242fa4bd7161cf
Elham Saboori <elham.saboori@unity3d.com>
2022-10-25 10:25:05 -0400

    error message class

---
+++ Unity/dt-hackathon-2022-temptwinURP/Assets/NestAPI/Scripts/ErrorResponse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Assets.NestAPI.Scripts
+{
+    [Serializable]
+    public class ErrorResponse
+    {
+        public Error error;
+    }
+
+    [Serializable]
+    public class Error
+    {
+        public string code;
+        public string message;
+        public string status;
+    }
+}
+
+
+//{
+//"error": {
+//    "code": 404,
+//    "message": "Device enterprises/project-id/devices/device-id not found.",
+//    "status": "NOT_FOUND"
+//  }
+//}
