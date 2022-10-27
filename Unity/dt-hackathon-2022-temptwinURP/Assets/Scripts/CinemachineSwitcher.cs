b3860941e04b4cd83f53b6cd384e222c17acb7ac
Jules <jules.dembele@unity3d.com>
2022-10-27 12:37:31 -0400

    FIx camera lock

--- Unity/dt-hackathon-2022-temptwinURP/Assets/Scripts/CinemachineSwitcher.cs
+++ Unity/dt-hackathon-2022-temptwinURP/Assets/Scripts/CinemachineSwitcher.cs
@@ -7,10 +7,10 @@ public class CinemachineSwitcher : MonoBehaviour
     [SerializeField]
     Animator m_CinemachineAnimator;
     [SerializeField]
-    bool m_IsPlayerCamera = false;
-    [SerializeField]
     StarterAssetsInputs m_StarterAssetsInputs;
-
+    
+    bool m_IsPlayerCamera = false;
+    
     void Awake()
     {
         m_CinemachineAnimator.GetComponent<Animator>();
@@ -21,6 +21,7 @@ public class CinemachineSwitcher : MonoBehaviour
         m_CinemachineAnimator.Play(m_IsPlayerCamera ? "ThermostatCamera" : "PlayerCamera");
         m_StarterAssetsInputs.cursorLocked = !m_IsPlayerCamera;
         m_StarterAssetsInputs.cursorInputForLook = !m_IsPlayerCamera;
+        m_StarterAssetsInputs.SetCursorState(!m_IsPlayerCamera);
         m_IsPlayerCamera = !m_IsPlayerCamera;
     }
 
